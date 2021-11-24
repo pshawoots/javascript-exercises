efc9a37492a7e6d68468028b3dd584ce625813d6
PaldenS <pshawoots@gmail.com>
2021-11-24 18:10:02 -0500

    completed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(stringInput, repeatString) {
+    let keepGoing = true
+    if(repeatString == 0){
+        keepGoing = false;
+    } else if(repeatString < 0) {
+        return "ERROR"
+    }
+    let returnString = "";
+    while(keepGoing) {
+        returnString += stringInput
+        repeatString--;
+        if(repeatString < 1) {
+            keepGoing = false;
+        }
+    }
+    return returnString
 };
 
 // Do not edit below this line
