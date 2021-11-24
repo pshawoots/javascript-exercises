ca0f94a39279f2875f027b8f4dd729af353d3698
PaldenS <pshawoots@gmail.com>
2021-11-24 18:38:19 -0500

    finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
 const reverseString = function(str) {
+    let finalString = "";
 
+    finalString = str.slice(-1,)
+
+    for(let i = 1; i < str.length; i++) {
+        let firstIndex = (i + 1) * -1
+        let secondIndex = i * -1
+        finalString += str.slice(firstIndex, secondIndex);
+    }
+    
+
+    return finalString
 };
 
 // Do not edit below this line
