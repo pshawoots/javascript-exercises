7ca95018c0a2fd8faf57437a8677eda00f6f3d74
PaldenS <pshawoots@gmail.com>
2021-11-24 19:40:04 -0500

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    if(typeof(firstNumber) != "number" || typeof(secondNumber) != "number" || firstNumber < 0 || secondNumber < 0) {
+        return "ERROR"
+    }
+    else if(firstNumber > secondNumber) {
+        let sum = 0;
+        for(let i = secondNumber; i <= firstNumber; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        let sum = 0;
+        for(let i = firstNumber; i <= secondNumber; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
