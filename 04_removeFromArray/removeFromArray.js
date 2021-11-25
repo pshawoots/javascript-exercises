ef1a54d89b68a2e87b081eb837f58c3e00ae26c6
PaldenS <pshawoots@gmail.com>
2021-11-24 19:30:47 -0500

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, toBeRemovedOne, toBeRemovedTwo, toBeRemovedThree,  toBeRemovedFour) {
+    let toBeRemovedArray = [toBeRemovedOne, toBeRemovedTwo, toBeRemovedThree, toBeRemovedFour];
+    let toBeRemoved = toBeRemovedArray.entries()
+    console.log("arrayBefore: " + array);
+    for (let toBeRemovedEntry of toBeRemoved) {
+        console.log("toBeRemovedEntry: " + toBeRemovedEntry[1]);
+        if(toBeRemovedEntry[1] == null) {
+            continue;
+        }
+        for(let i = 0; i < array.length; i++) {
+            console.log("array value " + array[i])
+            if(array[i] === toBeRemovedEntry[1]){
+                array.splice(i, 1)
+            }
+        }
+    }
+    console.log("arrayAfter: " + array)
+    return array;
 };
 
 // Do not edit below this line
