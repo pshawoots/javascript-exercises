de45e3f5e2a952e8bbb7515f41bfe10526cf4ecc
PaldenS <pshawoots@gmail.com>
2022-03-30 17:56:23 -0400

    completed 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(input)  {
+  let celsius = (input - 32) * 5/9;
+  celsius = Math.round(celsius * 10)/10;
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(input) {
+  let fahr = input * 9/5 + 32;
+  fahr = Math.round(fahr * 10)/10;
+  return fahr;
 };
 
 // Do not edit below this line
