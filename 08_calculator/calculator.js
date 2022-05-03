277d86bbfa5300468caf8f70c1cdfa33e8d5ae02
PaldenS <pshawoots@gmail.com>
2022-05-03 19:37:19 -0400

    completed exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(inputOne, inputTwo) {
+	return inputOne + inputTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+	return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	const total = numbers.reduce(function(total, number) {
+    return total += number;
+  }, 0);
+  
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+	const total = numbers.reduce(function(total, number) {
+    return total *= number;
+  }, 1);
+  
+  return total;
+};
+
+const power = function(base, power) {
+	// var answer = 1;
+  // for(var i = 0; i < power; i++) {
+  //   answer *= base;
+  // } 
+  // return answer;
+  const fillerArray = [...Array(power).keys()]
+  return fillerArray.reduce(function(total, num) {
+    return total *= base;
+  }, 1);
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(factorialStart) {
+	const fillers = [...Array(factorialStart).keys()];
+  return fillers.reduce(function(total, filler) {
+    return total *= (filler + 1);
+  }, 1);
 };
 
 // Do not edit below this line
