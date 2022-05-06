756791206da660b69568902021f7474a1e487da8
PaldenS <pshawoots@gmail.com>
2022-05-06 19:56:32 -0400

    completed exercise and added index.html for debugging in console

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n < 0) {
+        return "OOPS";
+    }
+    else if(n == 0) {
+        return 0;
+    } else if (n == 1)  {
+        return 1;
+    } else {
+        return fibonacci(n - 2) + fibonacci(n - 1);
+    } 
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
+    return persons.reduce(function(oldestPerson, person) {
+        person.age = 0;
+        if(!person.yearOfDeath) {
+            person.age = new Date().getFullYear() - person.yearOfBirth;
+        } else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+        return person.age > oldestPerson.age ? person:oldestPerson; 
 
+    },{yearOfBirth:0, yearOfDeath:0, age: 0,});
 };
 
 // Do not edit below this line
