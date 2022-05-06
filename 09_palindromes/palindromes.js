b268b3dae587da6603346f5c1aa2f6c3d0e34225
PaldenS <pshawoots@gmail.com>
2022-05-06 18:22:12 -0400

    completed exercise modified spec.js for even numbered palindrome and added index.html for testing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toUpperCase();
+    let stringArray = string.split("");
+    const letterOnlyArray = stringArray.filter(char => 
+        (char.charCodeAt(0) >= 'A'.charCodeAt(0)) 
+        && (char.charCodeAt(0) <= 'Z'.charCodeAt(0)) );
+    var isPalindrome = true;
+    var frontIndex = 0; 
+    var lastIndex = letterOnlyArray.length - 1;
 
+    while(isPalindrome && (frontIndex < lastIndex)) {
+        // console.log(letterOnlyArray[frontIndex]);
+        isPalindrome = (letterOnlyArray[frontIndex] == letterOnlyArray[lastIndex]);
+        frontIndex = frontIndex + 1;
+        lastIndex = lastIndex - 1;
+    }
+    return isPalindrome;
 };
 
 // Do not edit below this line
